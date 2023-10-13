ed4133035b3ea68d369b87976e369bde7f03c018
kimwest00 <minseohufs@hufs.ac.kr>
2023-10-13 19:12:53 +0900

    [feat] #17 add baseFragment

--- app/src/main/java/org/sesac/management/base/BaseFragment.kt
+++ app/src/main/java/org/sesac/management/base/BaseFragment.kt
@@ -1,4 +1,32 @@
 package org.sesac.management.base
 
-class BaseFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+typealias FragmentInflate<T> = (LayoutInflater, ViewGroup?, Boolean) -> T
+
+abstract class BaseFragment<VB : ViewBinding>(
+    private val inflate: FragmentInflate<VB>
+) : Fragment() {
+
+    var _binding: VB? = null
+    val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = inflate.invoke(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
