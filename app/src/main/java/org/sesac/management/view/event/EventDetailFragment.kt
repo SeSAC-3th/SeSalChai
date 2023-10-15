5988fede52da5d4b75bd27f2430b076db7fe2650
harry <harry7408@naver.com>
2023-10-15 18:07:00 +0900

    [Feat] #7 Schedule Detail UI 구현

--- app/src/main/java/org/sesac/management/view/event/EventDetailFragment.kt
+++ app/src/main/java/org/sesac/management/view/event/EventDetailFragment.kt
@@ -1,22 +1,32 @@
 package org.sesac.management.view.event
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import org.sesac.management.R
+import org.sesac.management.base.BaseFragment
+import org.sesac.management.databinding.FragmentEventDetailBinding
 
 
-class EventDetailFragment : Fragment() {
-
+class EventDetailFragment
+    : BaseFragment<FragmentEventDetailBinding>(FragmentEventDetailBinding::inflate) {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_event_detail, container, false)
+        _binding = FragmentEventDetailBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding) {
+            eventToolbar.tvTitle.text=getString(R.string.event_detail_toolbar_text)
+        }
     }
 
 }
--- app/src/main/java/org/sesac/management/view/event/EventFragment.kt
+++ app/src/main/java/org/sesac/management/view/event/EventFragment.kt
@@ -1,44 +1,26 @@
 package org.sesac.management.view.event
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import org.sesac.management.R
-import org.sesac.management.databinding.FragmentArtistBinding
+import org.sesac.management.base.BaseFragment
 import org.sesac.management.databinding.FragmentEventBinding
-import org.sesac.management.view.artist.ArtistDetailFragment
-import org.sesac.management.view.artist.ArtistEnrollFragment
 
 
-class EventFragment : Fragment() {
-    private lateinit var binding: FragmentEventBinding
+class EventFragment : BaseFragment<FragmentEventBinding>(FragmentEventBinding::inflate) {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        binding = FragmentEventBinding.inflate(inflater, container, false)
+        _binding = FragmentEventBinding.inflate(inflater, container, false)
 
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.detail.setOnClickListener {
-            childFragmentManager
-                .beginTransaction()
-                .add(binding.eventLayout.id, EventDetailFragment())
-                .addToBackStack(null)
-                .commitAllowingStateLoss()
-        }
-        binding.enroll.setOnClickListener {
-            childFragmentManager
-                .beginTransaction()
-                .add(binding.eventLayout.id, EventEnrollFragment())
-                .addToBackStack(null)
-                .commitAllowingStateLoss()
-        }
+
     }
 }
