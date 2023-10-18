47ac61e4d8c4b7d2e06caf01c77568c0391b83fb
harry <harry7408@naver.com>
2023-10-18 10:04:50 +0900

    [Feat]:#24 DialogAdapter add

--- app/src/main/java/org/sesac/management/view/event/ArtistAddDialogFragment.kt
+++ app/src/main/java/org/sesac/management/view/event/ArtistAddDialogFragment.kt
@@ -1,40 +1,39 @@
 package org.sesac.management.view.event
 
+import android.app.AlertDialog
 import android.app.Dialog
+import android.content.DialogInterface
 import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.DialogFragment
+import org.sesac.management.R
 
 class ArtistAddDialogFragment : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return (parentFragment.let {
             /*  TODO Dialog 생성 */
-//            val selectedItems = ArrayList<Int>() // Where we track the selected items
-//            val builder = AlertDialog.Builder(requireActivity().baseContext)
-//            builder.setTitle("가수 선택")
-//                .setMultiChoiceItems(
-//                    , null,
-//                    DialogInterface.OnMultiChoiceClickListener { dialog, which, isChecked ->
-//                        if (isChecked) {
-//                            // If the user checked the item, add it to the selected items
-//                            selectedItems.add(which)
-//                        } else if (selectedItems.contains(which)) {
-//                            // Else, if the item is already in the array, remove it
-//                            selectedItems.remove(which)
-//                        }
-//                    })
-//                // Set the action buttons
-//                .setPositiveButton(
-//                    "OK",
-//                    DialogInterface.OnClickListener { dialog, id ->
-//                      /* TODO 이벤트 처리 */
-//                    })
-//                .setNegativeButton(
-//                    R.string.cancel,
-//                    DialogInterface.OnClickListener { dialog, id ->
-//                        ...
-//                    })
-//
-//            builder.create()
+            val selectedItems = ArrayList<Int>() // Where we track the selected items
+            val builder = AlertDialog.Builder(requireActivity().baseContext)
+            builder.setView(
+                View.inflate(
+                    requireActivity().baseContext,
+                    R.layout.fragment_artist_add_dialog,
+                    null
+                )
+            )
+            builder.setTitle("가수 선택")
+                .setPositiveButton(
+                    "OK",
+                    DialogInterface.OnClickListener { dialog, id ->
+                        /* TODO 이벤트 처리 */
+                    })
+                .setNegativeButton(
+                    "cancel",
+                    DialogInterface.OnClickListener { dialog, id ->
+
+                    })
+
+            builder.create()
         } ?: throw IllegalStateException("Fragment Cannot be null ")) as Dialog
     }
 }
