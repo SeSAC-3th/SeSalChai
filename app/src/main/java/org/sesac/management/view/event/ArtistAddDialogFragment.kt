f054f8d15392e3c430962be67cedea5281b1ee89
harry <harry7408@naver.com>
2023-10-19 10:54:50 +0900

    [Chore] #24 make comment before rebase

--- app/src/main/java/org/sesac/management/view/event/ArtistAddDialogFragment.kt
+++ app/src/main/java/org/sesac/management/view/event/ArtistAddDialogFragment.kt
@@ -9,31 +9,32 @@ import androidx.fragment.app.DialogFragment
 import org.sesac.management.R
 
 class ArtistAddDialogFragment : DialogFragment() {
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        return (parentFragment.let {
-            /*  TODO Dialog 생성 */
-            val selectedItems = ArrayList<Int>() // Where we track the selected items
-            val builder = AlertDialog.Builder(requireActivity().baseContext)
-            builder.setView(
-                View.inflate(
-                    requireActivity().baseContext,
-                    R.layout.fragment_artist_add_dialog,
-                    null
-                )
-            )
-            builder.setTitle("가수 선택")
-                .setPositiveButton(
-                    "OK",
-                    DialogInterface.OnClickListener { dialog, id ->
-                        /* TODO 이벤트 처리 */
-                    })
-                .setNegativeButton(
-                    "cancel",
-                    DialogInterface.OnClickListener { dialog, id ->
+//    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+//        return (parentFragment.let {
+//              TODO Dialog 생성
+//            val selectedItems = ArrayList<Int>() // Where we track the selected items
+//            val builder = AlertDialog.Builder(requireActivity().baseContext)
+//            builder.setView(
+//                View.inflate(
+//                    requireActivity().baseContext,
+//                    R.layout.fragment_artist_add_dialog,
+//                    null
+//                )
+//            )
+//            builder.setTitle("가수 선택")
+//                .setPositiveButton(
+//                    "OK",
+//                    DialogInterface.OnClickListener { dialog, id ->
+//                         TODO 이벤트 처리
+//                    })
+//                .setNegativeButton(
+//                    "cancel",
+//                    DialogInterface.OnClickListener { dialog, id ->
+//
+//                    })
+//
+//            builder.create()
+//        } ?: throw IllegalStateException("Fragment Cannot be null ")) as Dialog
 
-                    })
-
-            builder.create()
-        } ?: throw IllegalStateException("Fragment Cannot be null ")) as Dialog
-    }
+//    }
 }
