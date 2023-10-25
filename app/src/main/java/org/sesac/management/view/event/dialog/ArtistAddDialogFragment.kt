2c930aec6fc9f30ff6f8926f482ec1d62a7964d9
Hyewon711 <84006341+Hyewon711@users.noreply.github.com>
2023-10-25 20:53:01 +0900

    [Feat] #79 Event ArtistAddDialogFragment.kt 구현중

--- app/src/main/java/org/sesac/management/view/event/dialog/ArtistAddDialogFragment.kt
+++ app/src/main/java/org/sesac/management/view/event/dialog/ArtistAddDialogFragment.kt
@@ -1,81 +1,54 @@
 package org.sesac.management.view.event.dialog
 
-import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.app.Dialog
 import android.content.DialogInterface
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.Window
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import org.sesac.management.R
 import org.sesac.management.data.model.DialogItem
 import org.sesac.management.databinding.FragmentArtistAddDialogBinding
+import org.sesac.management.view.artist.ArtistViewModel
 
 class ArtistAddDialogFragment : DialogFragment() {
-//    private lateinit var binding: FragmentArtistAddDialogBinding
-//    override fun onCreateView(
-//        inflater: LayoutInflater,
-//        container: ViewGroup?,
-//        savedInstanceState: Bundle?,
-//    ): View {
-//        binding = FragmentArtistAddDialogBinding.inflate(inflater, container, false)
-//        return binding.root
-//    }
+    private val viewModel: ArtistViewModel by activityViewModels()
+    private val selectedItems = mutableListOf<String>()
+    val TAG: String = "로그"
+    private lateinit var binding: FragmentArtistAddDialogBinding
 
-    //    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//
-//        binding = FragmentArtistAddDialogBinding.inflate(inflater, container, false)
-//        val dialog =  binding.root
-//        val testItems = mutableListOf(
-//            DialogItem("뉴진스", 1, false),
-//            DialogItem("아이브", 2, false),
-//            DialogItem("르세라핌", 3, false)
-//        )
-//
-//        val rvArtist = binding.rvArtist
-//        rvArtist.layoutManager = LinearLayoutManager(requireContext())
-//        val adapter = DialogAdapter(testItems)
-//        rvArtist.adapter = adapter
-//
-//
-//        val builder = AlertDialog.Builder(requireActivity().baseContext)
-//        builder.setTitle(resources.getString(R.string.event_select_artist))
-//        builder.setView(dialogLayout)
-//            .setPositiveButton(
-//                "선택",
-//                DialogInterface.OnClickListener { dialog, id ->
-//
-//                })
-//            .setNegativeButton(
-//                "닫기",
-//                DialogInterface.OnClickListener { dialog, id ->
-//
-//                })
-//
-//        builder.create()
-//        }
-//    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        this.binding = FragmentArtistAddDialogBinding.inflate(LayoutInflater.from(requireContext()))
+        /**
+         * 전체 아티스트 viewModel에서 가져오기
+         */
+        viewModel.getAllArtist.observe(viewLifecycleOwner) { artist ->
+            // 아티스트 목록을 가져온 후 RecyclerView에 표시
+            val rvArtist = this.binding.rvArtist
+            rvArtist.layoutManager = LinearLayoutManager(requireContext())
+            // artists를 DialogItem으로 변환
+            val dialogItems = artist.map { artist ->
+                DialogItem(artist.name, artist.artistId, selectedItems.contains(artist.name))
+            }
+            val adapter = DialogAdapter(dialogItems)
+            rvArtist.adapter = adapter
+        }
+    }
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val binding = FragmentArtistAddDialogBinding.inflate(LayoutInflater.from(requireContext()))
-        val dialogLayout = binding.root
+        this.binding = FragmentArtistAddDialogBinding.inflate(LayoutInflater.from(requireContext()))
+        val dialogLayout = this.binding.root
+
         dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         dialog?.window?.requestFeature(Window.FEATURE_NO_TITLE)
-
-        val testItems = mutableListOf(
-            DialogItem("뉴진스", 1, false),
-            DialogItem("아이브", 2, false),
-            DialogItem("르세라핌", 3, false)
-        )
-
-        val rvArtist = binding.rvArtist
-        rvArtist.layoutManager = LinearLayoutManager(requireContext())
-        val adapter = DialogAdapter(testItems)
-        rvArtist.adapter = adapter
 
 
         val builder = AlertDialog.Builder(requireContext())
@@ -84,20 +57,27 @@ class ArtistAddDialogFragment : DialogFragment() {
             .setPositiveButton(
                 "선택",
                 DialogInterface.OnClickListener { dialog, id ->
+                    // 선택된 아티스트 List 형태로 만들기
+                    val selectedArtists = selectedItems.toList()
+                    // 콜백 실행
+                    val listener = activity as CustomDialogListener
+                    listener.onArtistsSelected(selectedArtists)
 
+                    // Dialog 닫기
+                    dismiss()
                 })
             .setNegativeButton(
                 "닫기",
                 DialogInterface.OnClickListener { dialog, id ->
-
+                    // 다이얼로그를 닫는 작업
                 })
 
         builder.create()
         return builder.create()
     }
-
 }
 
-
-/*
-*/
+/**
+ * Dialog Callback을 위한 interface
+ * @author 혜원
+ */
