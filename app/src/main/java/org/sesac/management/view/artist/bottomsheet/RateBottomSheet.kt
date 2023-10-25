a807dbec41c01a98eadb0eeed135e0316cbbbed7
wbham <wbham97@naver.com>
2023-10-25 19:05:45 +0900

    [Refact] #85 - RXBinding 적용

--- app/src/main/java/org/sesac/management/view/artist/bottomsheet/RateBottomSheet.kt
+++ app/src/main/java/org/sesac/management/view/artist/bottomsheet/RateBottomSheet.kt
@@ -6,8 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import org.sesac.management.R
 import org.sesac.management.data.local.Rate
 import org.sesac.management.databinding.FragmentRateBottomSheetBinding
+import org.sesac.management.util.common.showToastMessage
+import org.sesac.management.util.extension.setOnAvoidDuplicateClickFlow
 import org.sesac.management.view.artist.ArtistViewModel
 
 class RateBottomSheet : BottomSheetDialogFragment() {
@@ -25,9 +28,10 @@ class RateBottomSheet : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         val bundle = arguments
         with(binding) {
-            btnSave.setOnClickListener {
+            btnSave.setOnAvoidDuplicateClickFlow {
                 val earnings = sliderRevenue.value.toInt()
                 val popularity = sliderPopularity.value.toInt()
                 val dance = sliderDance.value.toInt()
@@ -46,8 +50,12 @@ class RateBottomSheet : BottomSheetDialogFragment() {
                             0
                         ), data
                     )
+                    showToastMessage(resources.getString(R.string.artist_enroll_success))
+                    dismiss()
+                } else {
+                    showToastMessage(resources.getString(R.string.artist_enroll_fail))
+                    dismiss()
                 }
-                dismiss()
             }
         }
     }
