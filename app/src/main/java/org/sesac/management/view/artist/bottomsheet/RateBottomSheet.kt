02218a61f321254b2c724e83dbd2d1a184b4594b
wbham <wbham97@naver.com>
2023-10-26 23:27:48 +0900

    [Add] #89 - temp commit

--- app/src/main/java/org/sesac/management/view/artist/bottomsheet/RateBottomSheet.kt
+++ app/src/main/java/org/sesac/management/view/artist/bottomsheet/RateBottomSheet.kt
@@ -9,6 +9,7 @@ import androidx.annotation.RequiresApi
 import androidx.fragment.app.activityViewModels
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import org.sesac.management.R
+import org.sesac.management.data.local.Rate
 import org.sesac.management.databinding.FragmentRateBottomSheetBinding
 import org.sesac.management.util.common.showToastMessage
 import org.sesac.management.util.extension.setOnAvoidDuplicateClickFlow
@@ -44,17 +45,17 @@ class RateBottomSheet : BottomSheetDialogFragment() {
                     val performance = sliderPerform.value.toInt()
                     val sing = sliderSing.value.toInt()
 
-//                    viewModel.insertRateWithArtist(
-//                        Rate(
-//                            0f,
-//                            earnings,
-//                            popularity,
-//                            sing,
-//                            dance,
-//                            performance,
-//                            0
-//                        ), artistId
-//                    )
+                    viewModel.insertRateWithArtist(
+                        Rate(
+                            0f,
+                            earnings,
+                            popularity,
+                            sing,
+                            dance,
+                            performance,
+                            0
+                        ), artistId
+                    )
                     showToastMessage(resources.getString(R.string.artist_enroll_success))
                     dismiss()
                 }
