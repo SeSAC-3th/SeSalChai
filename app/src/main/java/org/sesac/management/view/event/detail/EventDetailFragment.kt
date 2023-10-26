a7ed321027ed334154a2d3215578e84e02ca566c
wbham <wbham97@naver.com>
2023-10-27 03:34:54 +0900

    [Fix] - 복구
    - ArtistDetailFragment 수정
    - EventDetailFragment 에러 수정
    - EventEditFragment 에러 수정

--- app/src/main/java/org/sesac/management/view/event/detail/EventDetailFragment.kt
+++ app/src/main/java/org/sesac/management/view/event/detail/EventDetailFragment.kt
@@ -36,7 +36,7 @@ class EventDetailFragment
     val TAG: String = "로그"
     private lateinit var viewPager: ViewPager2
     private var bannerPosition = 0
-    private val eventViewModel: EventViewModel by activityViewModels()
+    private val eventViewModel: EventViewModel by viewModels({ requireParentFragment() })
     private val artistViewModel: ArtistViewModel by activityViewModels()
     private lateinit var artistIdList: List<Int>
     private var eventId = 0
@@ -53,6 +53,7 @@ class EventDetailFragment
             eventId = event.eventId
             getEventDetail(event)
             eventViewModel.getArtistFromEvent(eventId)
+            updateUI(event)
         }
     }
 
@@ -63,19 +64,16 @@ class EventDetailFragment
                 getSelectArtist(artist)
             }
         }
-
+    }
 
     private fun updateUI(event : Event) {
         with(binding) {
-            event.imgUri?.let {
-                ivEvent.setImageBitmap(it)
-            }
+            ivEvent.setImageBitmap(event.imgUri)
             tvEventTitle.text=event.name
             tvEventTime.text=event.date.toString()
             tvEventPlace.text=event.place
             tvEventDescription.text=event.description
         }
-
     }
 
     @SuppressLint("UnsafeRepeatOnLifecycleDetector")
